6fc2576cdcab77850347f74f0548d7e5eea844a5
Andrew Schwimmer <andrew.schwimmer@disneystreaming.com>
2021-02-02 18:43:21 -0500

    Added Rocket and RocketTest

---
+++ src/Rocket.java
@@ -0,0 +1,50 @@
+public class Rocket {
+
+    private final double velocity;
+    private final double angle;
+
+    public Rocket(double velocity, double angle) {
+        this.velocity = velocity;
+        this.angle = Math.toRadians(angle);
+    }
+
+    /**
+     *
+     * @param time in seconds
+     * @return the X location of the Rocket at the specified time.
+     */
+    public double getX(double time) {
+        double xVelocity = Math.cos(angle) * velocity;
+        double x = xVelocity * time;
+        return x;
+    }
+
+    /**
+     *
+     * @param time in seconds
+     * @return the Y location of the Rocket at the specified time.
+     */
+    public double getY(double time) {
+        final double GRAVITY = 9.8;
+        double yVelocity = Math.sin(angle) * velocity;
+        double y = yVelocity * time - 0.5 * GRAVITY * time * time;
+        return y;
+    }
+
+    /**
+     *
+     * @return the time in seconds that the rocket lands.
+     */
+    public double getFlightTime() {
+        final double GRAVITY = 9.8;
+        double yVelocity = Math.sin(angle) * velocity;
+        return yVelocity / (0.5 * GRAVITY);
+    }
+
+    public double getHighestY() {
+        final double GRAVITY = 9.8;
+        double yVelocity = Math.sin(angle) * velocity;
+        return (yVelocity * yVelocity) / (GRAVITY * 2);
+    }
+
+}
---
+++ src/RocketTest.java
@@ -0,0 +1,80 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RocketTest {
+
+    @Test
+    public void getX() {
+        // given
+        Rocket rocket = new Rocket(71, 35);
+
+        // when
+        double x = rocket.getX(3);
+
+        // then
+        Assert.assertEquals(174.47, x, 0.01);
+    }
+
+    @Test
+    public void getY() {
+        // given
+        Rocket rocket = new Rocket(71, 35);
+
+        // when
+        double y = rocket.getY(3);
+
+        // then
+        Assert.assertEquals(78.07, y, 0.01);
+    }
+
+    @Test
+    public void timeToLand() {
+        // given
+        Rocket rocket = new Rocket(71, 35);
+
+        // when
+        double time = rocket.getTimeToLand();
+
+        // then
+        Assert.assertEquals(8.31, time, 0.01);
+    }
+
+    @Test
+    public void getY_land() {
+        // given
+        Rocket rocket = new Rocket(71, 35);
+
+        // when
+        double y = rocket.getY(8.31);
+
+        // then
+        Assert.assertEquals(0, y, 0.1);
+    }
+
+    @Test
+    public void getHighestY() {
+        // given
+        Rocket rocket = new Rocket(71, 35);
+
+        // when
+        double y = rocket.getHighestY();
+
+        // then
+        Assert.assertEquals(84.61, y, 0.01);
+    }
+
+    @Test
+    public void getY_highest() {
+        // given
+        Rocket rocket = new Rocket(71, 35);
+        double time = 8.31 / 2.0;
+
+        // when
+        boolean result = rocket.getY(time) > rocket.getY(time+0.1) &&
+                rocket.getY(time) > rocket.getY(time-0.1);
+
+        // then
+        Assert.assertTrue(result);
+    }
+
+}
