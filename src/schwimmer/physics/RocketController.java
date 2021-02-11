0b4f97adce653a6f4506e5f2b077cac2bdd56296
Andrew Schwimmer <andrew.schwimmer@disneystreaming.com>
2021-02-10 21:51:22 -0500

    added RocketApplication

---
+++ src/schwimmer/physics/RocketController.java
@@ -0,0 +1,24 @@
+package schwimmer.physics;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class RocketController {
+
+    @FXML
+    TextField velocityField;
+    @FXML
+    TextField angleField;
+    @FXML
+    TextField secondsField;
+
+    public void calculateRocket(ActionEvent actionEvent) {
+        double velocity = Double.parseDouble(velocityField.getText());
+        double angle = Double.parseDouble(angleField.getText());
+        double seconds = Double.parseDouble(secondsField.getText());
+
+        Rocket rocket = new Rocket(velocity, angle);
+        System.out.println(rocket.getFlightTime());
+    }
+}
